a6c4ca73ae1d65ba152548f2ab7d900e17fdaded
minyeong <minyk0512@gmail.com>
2025-01-26 18:15:18 +0900

    update: 메인페이지 태그, props 수정

--- src/app/components/ui/PrimaryLinkButton.tsx
+++ src/app/components/ui/PrimaryLinkButton.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import PrimaryButton from "./PrimaryButton";
 
-export default function PrimaryLinkButton({moveTo}: {moveTo: string}) {
+export default function PrimaryLinkButton({moveTo, label}: {moveTo: string, label?: string;}) {
   return (
-    <Link href={moveTo}>
+    <Link href={moveTo} aria-label={label}>
         <PrimaryButton>이동하기</PrimaryButton>
     </Link>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import PrimaryLinkButton from "./components/ui/PrimaryLinkButton";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col justify-center items-center gap-[30px]">
+    <main className="flex flex-col justify-center items-center gap-[30px]">
       <p className="text-white">
         동기부여/의미 설명 코멘트 이러이러한 맥락에서 합니다~
       </p>
       <PrimaryLinkButton moveTo='/punch/unreal/2'/>
-    </div>
+    </main>
   );
 }
