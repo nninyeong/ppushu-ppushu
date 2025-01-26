ed72b8d60e35b8b2fb17fc89ee7de0c831ed22a6
minyeong <minyk0512@gmail.com>
2025-01-26 17:23:18 +0900

    add: 테마 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,22 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        blue: {
+          300: '#2563eb',
+          200: '#598EF3',
+          100: '#D3E6FE'
+        },
+        pink: {
+          300: '#d946ef',
+          100: '#fae8ff'
+        },
+        dark: {
+          500: '#1e293b',
+          400: '#334155',
+          300: '#475569',
+          200: '#94a3b8',
+          100: '#cbd5e1'
+        }
       },
     },
   },
